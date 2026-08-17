b3f5f43378bc5ac109e2ae5f40937ffb45242a9d
ZAKI TRI PAMUNGKAS <113015706+ZAKITRIPAMUNGKAS@users.noreply.github.com>
2026-08-17 13:33:53 +0700

    fix(oauth): eliminate double slashes in google redirect uri

--- config/services.php
+++ config/services.php
@@ -38,7 +38,9 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_REDIRECT_URI', env('APP_URL') . '/auth/google/callback'),
+        'redirect' => env('GOOGLE_REDIRECT_URI') 
+            ? preg_replace('#(?<!:)//+#', '/', env('GOOGLE_REDIRECT_URI')) 
+            : rtrim(env('APP_URL', 'http://localhost:8000'), '/') . '/auth/google/callback',
     ],
 
 ];
